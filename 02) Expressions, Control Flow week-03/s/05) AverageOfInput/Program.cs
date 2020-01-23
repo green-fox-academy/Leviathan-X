de31853edb41da36fd65f8e6d2e9d07f8b670d90
Leviathan-X <Igor.Orlov@seznam.cz>
2020-01-23 16:02:16 +0100

    AverageOfInput update with arrays

--- 02) Expressions, Control Flow week-03/s/05) AverageOfInput/Program.cs
+++ 02) Expressions, Control Flow week-03/s/05) AverageOfInput/Program.cs
@@ -10,17 +10,29 @@ namespace AverageOfInput
             // then it should print the sum and the average of these numbers like:
             //
             // Sum: 22, Average: 4.4
-            Console.WriteLine("Greetings, please enter five variables to calculate their average: ");
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            Console.WriteLine("Greetings, \nplease enter five variables to calculate their average: \n");
+
+            int[] loopio = new int[5];
+
+            for (int i = 0; i < loopio.Length; i++) 
+            {
+                loopio[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nLoading..");
+
+            int sum = 0;
+            for (int i = 0; i < loopio.Length; i++)
+            {
+                sum += loopio[i];
+            }
+
+            int average = sum / loopio.Length;
+            Console.WriteLine($"\nSum: {sum}, Average: {average}");
+
+
+
+
             /*
             int val1;
             string input1;
