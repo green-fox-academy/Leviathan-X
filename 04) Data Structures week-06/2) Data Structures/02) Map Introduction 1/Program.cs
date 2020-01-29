020e92cb12382dde42232ca7f32cbf00fb678be2
Leviathan-X <Igor.Orlov@seznam.cz>
2020-01-29 11:27:25 +0100

    Lil' modify to Map Introduction 1

--- 04) Data Structures week-06/2) Data Structures/02) Map Introduction 1/Program.cs
+++ 04) Data Structures week-06/2) Data Structures/02) Map Introduction 1/Program.cs
@@ -9,55 +9,55 @@ namespace _02__Map_Introduction_1
         {
 
             // Create an empty map where the keys are integers and the values are characters
-            Dictionary<int, char> mydic = new Dictionary<int, char>();
+            Dictionary<int, char> myDic = new Dictionary<int, char>();
 
             // Print out whether the map is empty or not
-            Console.WriteLine($"\nIs the map empty: {mydic.Count.Equals(0)}");
+            Console.WriteLine($"\nIs the map empty: {myDic.Count.Equals(0)}");
 
             // Add the following key-value pairs to the map
-            mydic.Add(97, 'a');
-            mydic.Add(98, 'b');
-            mydic.Add(99, 'c');
-            mydic.Add(65, 'A');
-            mydic.Add(66, 'B');
-            mydic.Add(67, 'C');
+            myDic.Add(97, 'a');
+            myDic.Add(98, 'b');
+            myDic.Add(99, 'c');
+            myDic.Add(65, 'A');
+            myDic.Add(66, 'B');
+            myDic.Add(67, 'C');
 
             // Print all the keys
-            Console.WriteLine("\nAll the keys of mydic: ");
+            Console.WriteLine("\nAll the keys of myDic: ");
 
-            foreach (int key in mydic.Keys)
+            foreach (int key in myDic.Keys)
             {
                 Console.WriteLine(key);
             }
 
             // Print all the values
-            Console.WriteLine("\nAll the values of mydic: ");
+            Console.WriteLine("\nAll the values of myDic: ");
 
-            foreach (char value in mydic.Values)
+            foreach (char value in myDic.Values)
             {
                 Console.WriteLine(value);
             }
 
             // Add value D with the key 68
-            mydic.Add(68, 'D');
+            myDic.Add(68, 'D');
 
             // Print how many key-value pairs are in the map
-            Console.WriteLine($"\nHow many key-value pairs in mydic: { mydic.Count}");
+            Console.WriteLine($"\nHow many key-value pairs in myDic: {myDic.Count}");
 
             // Print the value that is associated with key 99
             char ch;
-            mydic.TryGetValue(99, out ch);
+            myDic.TryGetValue(99, out ch);
             Console.WriteLine($"\nValue of key 99: {ch}");
 
             // Remove the key-value pair with key 97
-            mydic.Remove(97);
+            myDic.Remove(97);
 
             // Print whether there is an associated value with key 100 or not
-            Console.WriteLine($"\nIs there a value with key of 100: {mydic.ContainsKey(100)}");
+            Console.WriteLine($"\nIs there a value with key of 100: {myDic.ContainsKey(100)}");
 
             // Remove all the key-value pairs
-            mydic.Clear();
-            Console.WriteLine($"\nIs the map empty: {mydic.Count.Equals(0)}");
+            myDic.Clear();
+            Console.WriteLine($"\nIs the map empty: {myDic.Count.Equals(0)}");
 
 
         }
