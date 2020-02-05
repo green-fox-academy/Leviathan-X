b0bf46a3822800ccdd760f6dd29e0fddc5e48cbe
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-05 18:08:12 +0100

    Add new exercise - Pokemon

---
+++ 07) Classes and Objects week-09/06) Pokemon/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06__Pokemon
+{
+    public enum PokemonType
+    {
+        Leaf,
+        Electric,
+        Fire,
+        Water
+    }
+    class Pokemon
+    {
+        public String Name { get; private set; }
+        public PokemonType Type { get; private set; }
+        public PokemonType EffectiveAgainst { get; private set; }
+
+        public Pokemon(String name, PokemonType type, PokemonType effectiveAgainst)
+        {
+            Name = name;
+            Type = type;
+            EffectiveAgainst = effectiveAgainst;
+        }
+
+        public bool IsEffectiveAgainst(Pokemon anotherPokemon)
+        {
+            return EffectiveAgainst == anotherPokemon.Type;
+        }
+    }
+}
---
+++ 07) Classes and Objects week-09/06) Pokemon/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06__Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pokemon> pokemonOfAsh = InitializePokemon();
+
+            // Every pokemon has a name and a type.
+            // Certain types are effective against others, e.g. water is effective against fire.
+
+            // Ash has a few pokemon.
+            // A wild pokemon appeared!
+
+            Pokemon wildPokemon = new Pokemon("Oddish", PokemonType.Leaf, PokemonType.Water);
+            Console.WriteLine($"\nA wild Pokemon appeared!\nName: {wildPokemon.Name}\nType: {wildPokemon.Type}\nEffective against: {wildPokemon.EffectiveAgainst}");
+
+            // Which pokemon should Ash use?
+            Console.WriteLine("\nWhich pokemon should Ash choose?\n\nLoading..");
+            
+            List<Pokemon> Choice = InitializePokemon();
+
+            foreach (var item in Choice)
+            {
+                if (item.IsEffectiveAgainst(wildPokemon)) Console.WriteLine($"\n\"I choose you, {item.Name}!\"");
+            }
+        }
+
+        private static List<Pokemon> InitializePokemon()
+        {
+            return new List<Pokemon>
+            {
+                new Pokemon("Balbasaur", PokemonType.Leaf, PokemonType.Water),
+                new Pokemon("Pikatchu", PokemonType.Electric, PokemonType.Water),
+                new Pokemon("Charizard", PokemonType.Fire, PokemonType.Leaf),
+                new Pokemon("Balbasaur", PokemonType.Water, PokemonType.Fire),
+                new Pokemon("Kingler", PokemonType.Water, PokemonType.Fire)
+            };
+        }
+    }
+}
