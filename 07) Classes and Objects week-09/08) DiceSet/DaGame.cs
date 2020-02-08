8f33b03d9abbffd556f71b310b65643e24c6e3ee
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-08 14:19:12 +0100

    Modify exercise - DiceSet

--- 07) Classes and Objects week-09/08) DiceSet/DaGame.cs
+++ 07) Classes and Objects week-09/08) DiceSet/DaGame.cs
@@ -38,38 +38,56 @@ namespace _08__DiceSet
                 else if (userInput == ConsoleKey.R) //|| userInput == "R")
                 {
                     Console.Write("\nWhich dice to roll?\n\nYour choice: ");
-                    string userChoice = Console.ReadLine();
-                    // Instead of switch parse. Copypasting is never good
-                    switch (userChoice)
+                    // Mishi: "Instead of switch use parse. Copypasting is never good."
+                    //string userChoice = Console.ReadLine();
+                    try
                     {
-                        case "1":
-                            dices.Reroll(0);
-                            Console.WriteLine("-- Dice 1 has been rolled! --");
-                            break;
-                        case "2":
-                            dices.Reroll(1);
-                            Console.WriteLine("-- Dice 2 has been rolled! --");
-                            break;
-                        case "3":
-                            dices.Reroll(2);
-                            Console.WriteLine("-- Dice 3 has been rolled! --");
-                            break;
-                        case "4":
-                            dices.Reroll(3);
-                            Console.WriteLine("-- Dice 4 has been rolled! --");
-                            break;
-                        case "5":
-                            dices.Reroll(4);
-                            Console.WriteLine("-- Dice 5 has been rolled! --");
-                            break;
-                        case "6":
-                            dices.Reroll(5);
-                            Console.WriteLine("-- Dice 6 has been rolled! --");
-                            break;
-                        default:
-                            Console.WriteLine("Error! Wrong input!\nPlease choose between the N, R or Q buttons.");
-                            break;
+                        int userReroll = Int32.Parse(Console.ReadLine());
+                        if (userReroll > 6 || userReroll < 0)
+                        {
+                            Console.WriteLine("Error! Wrong input!\nPlease choose between 1 and 6.");
+                        }
+                        else
+                        {
+                            dices.Reroll(userReroll - 1);
+                            Console.WriteLine($"-- Dice {userReroll - 1} has been rolled! --");
+                        }
                     }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Error! Wrong input!\nPlease choose between 1 and 6.");
+                    }
+                    // My old code:
+                    //switch (userChoice)
+                    //{
+                    //    case "1":
+                    //        dices.Reroll(0);
+                    //        Console.WriteLine("-- Dice 1 has been rolled! --");
+                    //        break;
+                    //    case "2":
+                    //        dices.Reroll(1);
+                    //        Console.WriteLine("-- Dice 2 has been rolled! --");
+                    //        break;
+                    //    case "3":
+                    //        dices.Reroll(2);
+                    //        Console.WriteLine("-- Dice 3 has been rolled! --");
+                    //        break;
+                    //    case "4":
+                    //        dices.Reroll(3);
+                    //        Console.WriteLine("-- Dice 4 has been rolled! --");
+                    //        break;
+                    //    case "5":
+                    //        dices.Reroll(4);
+                    //        Console.WriteLine("-- Dice 5 has been rolled! --");
+                    //        break;
+                    //    case "6":
+                    //        dices.Reroll(5);
+                    //        Console.WriteLine("-- Dice 6 has been rolled! --");
+                    //        break;
+                    //    default:
+                    //        Console.WriteLine("Error! Wrong input!\nPlease choose between the N, R or Q buttons.");
+                    //        break;
+                    //}
                     num++;
                 }
                 else if (userInput == ConsoleKey.Q) //|| userInput == "Q")
@@ -79,7 +97,7 @@ namespace _08__DiceSet
                 }
                 else
                 {
-                    Console.WriteLine("\nWrong input!\nPlease choose between 1 and 3.");
+                    Console.WriteLine("\nError! Wrong input!\nPlease choose between the N, R or Q buttons.");
                 }
             } while (consent == "n" || consent == "N" || consent == "Let's roll!");
         }
