41fb055947116b2d27e853c5fe51525e392348b6
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-15 13:16:12 +0100

    Modify exercise - The Garden Application

--- 09) Inheritance week-11/02) Inheritance/02) The Garden Application/Garden.cs
+++ 09) Inheritance week-11/02) Inheritance/02) The Garden Application/Garden.cs
@@ -7,14 +7,19 @@ namespace _02__The_Garden_Application
     class Garden
     {
         private string name;
+        List<Flower> flowers;
+        List<Tree> trees;
+
         public Garden(string Name)
         {
             name = Name;
+            flowers = new List<Flower>();
+            trees = new List<Tree>();
         }
 
         public void Watering(int water)
         {
-
+            if ()
         }
     }
 }
