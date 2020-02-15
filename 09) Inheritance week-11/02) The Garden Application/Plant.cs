d7c5fa912613b8b9099ff0fdb34e4e27810ad2f2
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-15 12:57:30 +0100

    Add new exercise - The Garden Application

---
+++ 09) Inheritance week-11/02) The Garden Application/Plant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02__The_Garden_Application
+{
+    class Plant
+    {
+        protected string color;
+        protected float waterAmount = 0f;
+        public Plant(string Color)
+        {
+            color = Color;
+        }
+
+        public virtual void Status()
+        {
+            if (waterAmount <= 6)
+            {
+                Console.WriteLine($"\nThe plant {color} needs water!");
+            } 
+            else
+            {
+                Console.WriteLine($"\nThe plant {color} doesn't need water.");
+            }
+        } 
+    }
+}
---
+++ 09) Inheritance week-11/02) The Garden Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02__The_Garden_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
