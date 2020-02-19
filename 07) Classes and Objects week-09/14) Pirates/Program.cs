30d123ab7b2db0ea1e5befc243595cbad3c4ea0d
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-19 14:32:40 +0100

    Modify exercise - Pirates

--- 07) Classes and Objects week-09/14) Pirates/Program.cs
+++ 07) Classes and Objects week-09/14) Pirates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _14__Pirates
 {
@@ -6,6 +8,56 @@ namespace _14__Pirates
     {
         static void Main(string[] args)
         {
+
+            Ship maryAnn = new Ship("Mary Ann");
+
+            Captain crewCap = new Captain("Cap'n");
+            maryAnn.FillShip(crewCap);
+            Pirate crewHand1 = new Pirate("Bob");
+            maryAnn.FillShip(crewHand1);
+            Pirate crewHand2 = new Pirate("Slime");
+            maryAnn.FillShip(crewHand2);
+            Pirate crewHand3 = new Pirate("Fartbubble");
+            maryAnn.FillShip(crewHand3);
+            Pirate crewHand4 = new Pirate("Sunshine");
+            maryAnn.FillShip(crewHand4);
+
+            crewCap.HowsItGoingMate();
+            crewHand1.DrinkSomeRum();
+            crewHand2.DrinkSomeRum();
+            crewHand3.DrinkSomeRum();
+
+            for (int i = 0; i < 5; i++)
+            {
+                crewHand4.DrinkSomeRum();
+            }
+            crewHand4.HowsItGoingMate();
+
+            crewHand1.Brawl(crewHand2);
+
+            maryAnn.GetStatus();
+
+
+            Ship bombardia = new Ship("Bombardia");
+
+            Captain crewCap0 = new Captain("Kommandant");
+            bombardia.FillShip(crewCap0);
+            Pirate crewHand01 = new Pirate("Eins");
+            bombardia.FillShip(crewHand01);
+            Pirate crewHand02 = new Pirate("Zwei");
+            bombardia.FillShip(crewHand02);
+            Pirate crewHand03 = new Pirate("Drei");
+            bombardia.FillShip(crewHand03);
+            Pirate crewHand04 = new Pirate("Vier");
+            bombardia.FillShip(crewHand04);
+
+            for (int i = 0; i < 4; i++)
+            {
+                crewCap0.DrinkSomeRum();
+            }
+            crewCap0.HowsItGoingMate();
+
+            bombardia.GetStatus();
 
         }
     }
---
+++ 07) Classes and Objects week-09/14) Pirates/Ship.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14__Pirates
+{
+    class Ship
+    {
+        List<Pirate> pirateCrew;
+        private string shipName;
+
+        public Ship(string ShipName)
+        {
+            shipName = ShipName;
+            pirateCrew = new List<Pirate>();
+            Console.WriteLine($"\n\n---- {shipName} sets sail! ----");
+        }
+
+        public void FillShip(Pirate crewMember)
+        {
+
+            // DUNNO HOW: 
+            // How to auto-generate objects and to distinguish between them
+
+            // Random randomValue = new Random();
+            // int numOfCrew = randomValue.Next(2, 11);
+
+            // Captain crewCap = new Captain("Cap'n");
+            // pirateCrew.Add(crewCap);
+
+            // for (int i = 0; i < numOfCrew; i++)
+            // {
+            //     Pirate crewHand = new Pirate("Bob");
+            //     pirateCrew.Add(crewHand);
+            // }
+
+            pirateCrew.Add(crewMember);
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine($"\n * * * *\nStatus of the {shipName} ship:\n\nCaptain's Rum Consuption: {Captain.CapIntoxication} per mil\nTotal Crew Passed Out: {Pirate.passedOutTotal}" +
+                $"\nNumber of Pirates Alive: {Pirate.AliveTotal}/{pirateCrew.Count} (Including captain)");
+        }
+
+        public void Battle(Ship enemyShip)
+        {
+            int ourScore = Pirate.AliveTotal - Captain.CapIntoxication;
+            //int theirScore = 
+
+
+        }
+    }
+}
