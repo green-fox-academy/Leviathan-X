eb09ec417b4f85bc699fd41155b44d3370af2889
Leviathan-X <Igor.Orlov@seznam.cz>
2020-02-29 14:29:36 +0100

    Add new exercise - Apples

---
+++ 11) Testing/01) Apples/Apple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01__Apples
+{
+    class Apples
+    {
+        private string apple;
+
+        public Apples()
+        {
+            apple = "apple";
+        }
+
+        public string GetApple()
+        {
+            return apple;
+        }
+    }
+}
---
+++ 11) Testing/01) Apples/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace _01__Apples
+{
+
+    //Create a class, with one method(eg. public string GetApple()) that returns a string (eg. "apple")
+    //Create a test for that.
+    //Create an xUnit project
+    //Add a new Test case in it
+    //Instantiate your class (arrange, act, assert)
+    //Use the Assert.Equal()
+    //The expected parameter should be the same string (eg. "apple")
+    //The actual parameter should be the return value of the method(eg.myObject.GetApple())
+    //Run the test
+    //Change the expected value to make the test failing
+    //Run the test
+    //Fix the returned value to make the test succeeding again
+
+    public class Class1
+    {
+        Apples apple = new Apples();
+
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal("apple", apple.GetApple());
+        }
+    }
+}
