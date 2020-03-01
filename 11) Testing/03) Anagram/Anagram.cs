3114f595b22e385da1aabfd682d70850ad5c121a
Leviathan-X <Igor.Orlov@seznam.cz>
2020-03-01 18:58:45 +0100

    Modify exercise - Anagram

--- 11) Testing/03) Anagram/Anagram.cs
+++ 11) Testing/03) Anagram/Anagram.cs
@@ -52,7 +52,7 @@ namespace _03__Anagram
             Console.WriteLine("\n");
             if(ExampleObject == true)
             {
-                Console.WriteLine("-- Example: ");
+                Console.WriteLine("-- Example: \n");
             }
             Console.Write($"Words to Compare: \"{Word1}\" and \"{Word2}\"");
             
@@ -60,6 +60,56 @@ namespace _03__Anagram
             
             Console.WriteLine($"\nWord Base: \t  \"{Word1Base}\" and \"{Word2Base}\"" +
                 $"\n\nAre these words Anagrams? \nResult: -- {result}! --");
+        }
+
+        public static void ExampleAnagram()
+        {
+            //string input1 = "Listen";
+            //string input2 = "Silent";
+            string input1 = "Mother In Law";
+            string input2 = "Woman Hitler";
+
+            var derNeueAnagrame = new Anagram(input1, input2, true);
+            derNeueAnagrame.CompareWords();
+        }
+
+        private static void TheLoop()
+        {
+            string consent = "yes";
+            do
+            {
+                Console.Write("\n\nContinue? (y/n): ");
+                var userInput = Console.ReadKey().Key;
+                if (userInput == ConsoleKey.N)
+                {
+                    consent = "no";
+                    Console.WriteLine("\n\n-----------------\nHave a nice day!");
+                }
+                else if (userInput == ConsoleKey.Y)
+                {
+                    string userWord1;
+                    string userWord2;
+
+                    Console.WriteLine("\n\n-------------------------------------------------------------- " +
+                        "\nPlease write your word or set of words you'd like to compare: ");
+                    Console.Write($"Your first word/s:  ");
+                    userWord1 = Console.ReadLine();
+                    Console.Write($"Your second word/s: ");
+                    userWord2 = Console.ReadLine();
+
+                    var userAnagram = new Anagram(userWord1, userWord2, false);
+                    userAnagram.CompareWords();
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease try again. Press Y or N.");
+                }
+            } while (consent == "yes");
+        }
+
+        public static void StartLoop()
+        {
+            TheLoop();
         }
     }
 }
--- 11) Testing/03) Anagram/Program.cs
+++ 11) Testing/03) Anagram/Program.cs
@@ -7,45 +7,10 @@ namespace _03__Anagram
         static void Main(string[] args)
         {
             Console.WriteLine("\n** Welcome to the Anagram Comparer! **");
+            Anagram.ExampleAnagram();
 
-            //string input1 = "Listen";
-            //string input2 = "Silent";
-            string input1 = "Mother In Law";
-            string input2 = "Woman Hitler";
-
-            var derNeueAnagrame = new Anagram(input1, input2, true);
-            derNeueAnagrame.CompareWords();
-
-            Console.WriteLine("\n\nFeel free to try your own Anagrams.");
-            string consent = "yes";
-            do
-            {
-                Console.Write("\n\nContinue? (y/n): ");
-                var userInput = Console.ReadKey().Key;
-                if (userInput == ConsoleKey.N)
-                {
-                    consent = "no";
-                    Console.WriteLine("\n\nHave a nice day!");
-                }
-                else if (userInput == ConsoleKey.Y)
-                {
-                    string userWord1;
-                    string userWord2;
-
-                    Console.WriteLine("\n\nPlease write your word or set of words you'd like to compare: ");
-                    Console.Write($"Your first word/s:  ");
-                    userWord1 = Console.ReadLine();
-                    Console.Write($"Your second word/s: ");
-                    userWord2 = Console.ReadLine();
-
-                    var userAnagram = new Anagram(userWord1, userWord2, false);
-                    userAnagram.CompareWords();
-                }
-                else 
-                {
-                    Console.WriteLine("\nPlease try again. Press Y or N.");
-                }
-            } while (consent == "yes");
+            Console.WriteLine("\nFeel free to try out your own Anagrams.");
+            Anagram.StartLoop();
         }
     }
 }
