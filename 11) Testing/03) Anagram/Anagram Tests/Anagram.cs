7759011b3a9faa5715aa1625b015f037ed72646c
Leviathan-X <Igor.Orlov@seznam.cz>
2020-03-01 13:39:18 +0100

    Modify exercise - Anagram

---
+++ 11) Testing/03) Anagram/Anagram Tests/Anagram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram_Tests
+{
+    class Anagram
+    {
+        private string Word1;
+        private string Word2;
+        private char[] Word1Modify;
+        private char[] Word2Modify;
+
+        private string Word1Base;
+        private string Word2Base;
+
+        public Anagram(string word1, string word2)
+        {
+            Word1 = word1;
+            Word2 = word2;
+        }
+
+        public bool SortInput()
+        {
+            Word1 = Word1.Replace(" ", string.Empty);   // "Word1.Trim()" doesn't do anything for some reason.
+            Word2 = Word2.Replace(" ", string.Empty);
+
+            Word1 = Word1.ToLower();
+            Word2 = Word2.ToLower();
+
+            Word1Modify = Word1.ToCharArray();
+            Array.Sort(Word1Modify);
+            // Word1 = Word1Modify.ToString();      - BIG NONO! Caused a real mess!
+            Word1Base = new string(Word1Modify);    // Use this instead!
+
+            Word2Modify = Word2.ToCharArray();
+            Array.Sort(Word2Modify);
+            // Word2 = Word2Modify.ToString();      - BIG NONO! Caused a real mess!
+            Word2Base = new string(Word2Modify);    // Use this instead! 
+            // Maybe it's because of the rule that "strings can't be changed"? Or something?
+
+            if (Word1Base == Word2Base) return true;
+            else return false;
+        }
+    }
+}
---
+++ 11) Testing/03) Anagram/Anagram Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Anagram_Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("Cold Taco", "coat clod")]
+        public void TestMethod1(string value1, string value2)
+        {
+            var newTest = new Anagram(value1, value2);
+            Assert.True(newTest.SortInput());
+        }
+    }
+}
