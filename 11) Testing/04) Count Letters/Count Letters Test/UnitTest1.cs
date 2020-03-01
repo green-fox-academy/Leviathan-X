f46def6d9f9037a2c34a1302614118255913a540
Leviathan-X <Igor.Orlov@seznam.cz>
2020-03-01 21:21:58 +0100

    Add new exercise - Count Letters

---
+++ 11) Testing/04) Count Letters/Count Letters Test/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Count_Letters_Test
+{
+    public class UnitTest1
+    {
+        public Dictionary<char, int> StartCount(string input)
+        {
+            Dictionary<char, int> myDic = new Dictionary<char, int>();
+            char[] chario = input.ToCharArray();
+
+            foreach (var item in chario)
+            {
+                if (myDic.ContainsKey(item))
+                {
+                    myDic[item] += 1;
+                }
+                else
+                {
+                    myDic.Add(item, 1);
+                }
+            }
+            return myDic;
+        }
+
+        [Theory]
+        [InlineData("Yo yo yo, son!")]
+        public void RunTest(string testInput)
+        {
+            Dictionary<char, int> myWonderfulDic = StartCount(testInput);
+
+            // Console.WriteLine("\nCloser Look At myDic:" +
+            //    "\nKey is for Letter, Value is for Number Of Times Used.\n");
+            int num = 1;
+            int totes = 0;
+            foreach (char key in myWonderfulDic.Keys)
+            {
+                int value;
+                myWonderfulDic.TryGetValue(key, out value);
+                totes += value;
+                // Console.WriteLine($"{num}.\nKey: \"{key}\" Value: {value}");
+                num++;
+            }
+            // Console.WriteLine($"\n--Total letters: {totes}");
+
+            //Assert.True(totes == 14);
+            Assert.Equal(14, totes);
+        }
+    }
+}
---
+++ 11) Testing/04) Count Letters/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _04__Count_Letters
+{
+    class Program
+    {
+        public static Dictionary<char, int> StartCount(string input)
+        {
+            Dictionary<char, int> myDic = new Dictionary<char, int>();
+            char[] chario = input.ToCharArray();
+
+            foreach (var item in chario)
+            {
+                if (myDic.ContainsKey(item))
+                {
+                    myDic[item] += 1;
+                }
+                else
+                {
+                    myDic.Add(item, 1);
+                }
+            }
+            return myDic;
+        }
+
+        static void Main(string[] args)
+        {
+            string nonsense = "Dude, you've got a really dirty mind. What's wrong with you?";
+            Dictionary<char, int> myWonderfulDic = StartCount(nonsense);
+            
+            Console.WriteLine("\nCloser Look At myDic:" +
+                "\nKey is for Letter, Value is for Number Of Times Used.\n");
+            int num = 1;
+            int totes = 0;
+            foreach (char key in myWonderfulDic.Keys)
+            {
+                int value;
+                myWonderfulDic.TryGetValue(key, out value);
+                totes += value;
+                Console.WriteLine($"{num}.\nKey: \"{key}\" Value: {value}");
+                num++;
+            }
+            Console.WriteLine($"\n--Total letters: {totes}");
+        }
+    }
+}
