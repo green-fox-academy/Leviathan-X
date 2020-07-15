e0c44de349946e67a90ae6bc117550973d4778ce
Leviathan-X <Igor.Orlov@seznam.cz>
2020-07-15 11:16:21 +0200

    Modify exercise - ListingToDos: Add assignee service & html page

---
+++ Java Orientation/09) Database Integration/01ListingToDos/src/main/java/com/greenfoxacademy/greenfoxacademy/services/AssServiceImpl.java
@@ -0,0 +1,56 @@
+package com.greenfoxacademy.greenfoxacademy.services;
+
+import com.greenfoxacademy.greenfoxacademy.models.Assignee;
+import com.greenfoxacademy.greenfoxacademy.models.ToDo;
+import com.greenfoxacademy.greenfoxacademy.repositories.AssRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AssServiceImpl implements AssService {
+
+    AssRepository assRepository;
+
+    @Autowired
+    public AssServiceImpl(AssRepository assRepository) {
+        this.assRepository = assRepository;
+    }
+
+    @Override
+    public List<Assignee> getAll() {
+        return this.assRepository.findAll();
+    }
+
+    @Override
+    public void createAssignee(String name, String email) {
+        Assignee assignee = new Assignee(name, email);
+        this.assRepository.save(assignee);
+    }
+
+    @Override
+    public void deleteAssignee(long id) {
+        this.assRepository.deleteById(id);
+    }
+
+    @Override
+    public Assignee getAssignee(long id) {
+        return this.assRepository.getOne(id);
+    }
+
+    @Override
+    public void updateAssignee(long id, String name, String email) {
+        Assignee assignee = new Assignee(id, name, email);
+        assignee.setId(id);
+        this.assRepository.save(assignee);
+    }
+
+    @Override
+    public List<Assignee> search(String name) {
+        return this.assRepository.findAll().stream()
+                .filter(toDo -> toDo.getName().toLowerCase().contains(name.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+}
