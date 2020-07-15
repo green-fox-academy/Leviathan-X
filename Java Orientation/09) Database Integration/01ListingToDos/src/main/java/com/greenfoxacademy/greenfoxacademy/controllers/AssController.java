4cbca0a2b4a35e01bd1f2c82d1dffa3ac7e905da
Leviathan-X <Igor.Orlov@seznam.cz>
2020-07-15 15:30:30 +0200

    Modify exercise - ListingToDos: add more html pages

--- Java Orientation/09) Database Integration/01ListingToDos/src/main/java/com/greenfoxacademy/greenfoxacademy/controllers/AssController.java
+++ Java Orientation/09) Database Integration/01ListingToDos/src/main/java/com/greenfoxacademy/greenfoxacademy/controllers/AssController.java
@@ -1,10 +1,73 @@
 package com.greenfoxacademy.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.greenfoxacademy.models.Assignee;
+import com.greenfoxacademy.greenfoxacademy.services.AssService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/assignees")
 public class AssController {
 
+    private AssService assService;
+
+    @Autowired
+    public AssController(AssService assService) {
+        this.assService = assService;
+    }
+
+    @GetMapping("/")
+    public String listRedirect() {
+        return "redirect:/assignees/list";
+    }
+
+    @GetMapping({"/list"})
+    public String list(Model model) {
+        List<Assignee> assignees = assService.getAll();
+        model.addAttribute("assignees", assignees);
+        return "asslist";
+    }
+
+    @GetMapping("/add-assignee")
+    public String addAssigneeGET() {
+        return "add-assignee";
+    }
+
+    @PostMapping("/add-assignee")
+    public String addAssigneePOST(@ModelAttribute("assignee") Assignee assignee) {
+        assService.createAssignee(assignee.getName(), assignee.getEmail());
+        return "redirect:/assignees/";
+    }
+
+    @GetMapping("/delete-assignee/{id}")
+    public String deleteAssignee(Model model, @PathVariable long id) {
+        assService.deleteAssignee(id);
+        return "redirect:/assignees/";
+    }
+
+    @GetMapping("/edit-assignee/{id}")
+    public String editAssigneeGET(Model model, @PathVariable long id) {
+        model.addAttribute("assignee", assService.getAssignee(id));
+        return "edit-assignee";
+    }
+
+    @PostMapping("/edit-assignee")
+    public String editAssigneePOST(@ModelAttribute("assignee") Assignee assignee,
+                               @RequestParam(value = "id") long id) {
+        assService.updateAssignee(id, assignee.getName(), assignee.getEmail());
+        return "redirect:/assignees/";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam String name) {
+        if (name != null) {
+            model.addAttribute("assignees", assService.search(name));
+            return "asslist";
+        }
+        else return "redirect:/assignees/";
+    }
 }
