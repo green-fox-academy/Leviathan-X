7b455bfc9f4e6fcd6c85a960c8a6b8ba8dc3f260
Leviathan-X <Igor.Orlov@seznam.cz>
2020-08-13 10:39:54 +0200

    Modify exercise - Creating, Inserting & Removing Elements

--- JavaScript Project Phase/04) DOM/05) Creating, Inserting & Removing Elements/createelement-3.js
+++ JavaScript Project Phase/04) DOM/05) Creating, Inserting & Removing Elements/createelement-3.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const petsList = document.querySelector('.pets');
+const petsList = document.querySelector('#pets');
 
 const kids = [
     { 'petName': 'Wattled crane', 'owner': 'Bryant' },
@@ -16,15 +16,27 @@ const kids = [
     { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
   ];
 
-// 1) Create an <article> element for each kid
+const owners = []
 Array.from(kids).forEach(input => {
+    // 1) Create an <article> element for each kid
     const newKid = document.createElement('article');
-    newKid.textContent = input.owner;
-    petsList.appendChild(newKid);
-    console.log(newKid);
+    //newKid.textContent = input.owner;
+
+    // 2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
+    //       - The H3 should contain the owner's name
+    //       - The p should contain the pet's name
+    const newH3 = document.createElement('h3');
+    newH3.textContent = input.owner;
+    const newP = document.createElement('p');
+    newP.textContent = input.petName;
+    
+    newKid.appendChild(newH3);
+    newKid.appendChild(newP);
+
+    owners.push(newKid);
 })
 
-// 2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
-//       - The H3 should contain the owner's name
-//       - The p should contain the pet's name
 // 3) Add the article to the pets div.
+Array.from(owners).forEach(input => {
+  petsList.appendChild(input);
+})
